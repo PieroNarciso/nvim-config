03f3738e1ff4268e7e9fda6ee6fa9b170d2816ed
Piero Narciso <pieronarciso988@gmail.com>
2022-02-22 21:47:26 -0500

    Added html lsp config

--- nvim-config/.config/nvim/lua/lsp/html.lua
+++ nvim-config/.config/nvim/lua/lsp/html.lua
@@ -1,10 +1,6 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 return {
-  filetypes = { "html" },
-  init_options = {
-    configurationSection = { "html", "css", "javascript" },
-    embeddedLanguages = {
-      css = true,
-      javascript = true
-    }
-  },
+  capabilities = capabilities,
 }
