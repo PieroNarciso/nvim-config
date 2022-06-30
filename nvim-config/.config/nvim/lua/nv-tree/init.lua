143330228ff4d6747742952f963c413511b49ab4
Piero Narciso <pieronarciso988@gmail.com>
2022-06-30 08:20:33 -0500

    update: nvimtree config (deprecated settings)

--- nvim-config/.config/nvim/lua/nv-tree/init.lua
+++ nvim-config/.config/nvim/lua/nv-tree/init.lua
@@ -12,7 +12,7 @@ require('nvim-tree').setup {
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab         = false,
   -- hijacks new directory buffers when they are opened.
-  update_to_buf_dir   = {
+  hijack_directories   = {
     -- enable the feature
     enable = true,
     -- allow to open the tree if it was previously closed
@@ -59,7 +59,7 @@ require('nvim-tree').setup {
     -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
     side = 'right',
     -- if true the tree will resize itself after opening a file
-    auto_resize = false,
+    adaptive_size = false,
     mappings = {
       -- custom only false will merge the list with the default mappings
       -- if true, it will only use your list to set the mappings
