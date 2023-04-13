1a2cdfa64b113d3d4cff92e17a13a0c01f379fb4
Piero Narciso <pieronarciso988@gmail.com>
2023-04-12 21:01:09 -0500

    update: nvim tree setup invalid options

--- nvim-config/.config/nvim/lua/nv-tree/init.lua
+++ nvim-config/.config/nvim/lua/nv-tree/init.lua
@@ -3,10 +3,6 @@ require('nvim-tree').setup {
   disable_netrw       = true,
   -- hijack netrw window on startup
   hijack_netrw        = true,
-  -- open the tree when running this setup function
-  open_on_setup       = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
   -- closes neovim automatically when the tree is the last **WINDOW** in the view
   -- auto_close          = false,
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
